fbedbaed83cce76f0f728400d110f3228e9b70b7
timoteifrincuu <timotei.frincu@emanuel.ro>
2024-11-22 01:24:59 +0200

    dist folder

--- davidthebarber/vite.config.js
+++ davidthebarber/vite.config.js
@@ -1,5 +1,20 @@
-import { defineConfig } from 'vite';
-
+import { defineConfig } from 'vite'
 export default defineConfig({
-  base: '/<DavidTheBarber>/',
-});
+    base: '',
+    build: {
+        rollupOptions: {
+            input: [
+                "/index.html",
+                "/appointment.html",
+                "/about.html",
+                "/account.html",
+                "/services.html",
+                "/log.html",
+                "/sign.html",
+                "/gallery.html",
+                "/reviews.html",
+                "/contact.html",
+            ]
+        }
+    }
+})
